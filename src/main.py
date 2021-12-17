c383d75cbc146db77b9b7954936543500f25f40c
StefanoMarchesi <95907613+StefanoMarchesin@users.noreply.github.com>
2021-12-17 10:56:22 +0000

    roba

--- src/main.py
+++ src/main.py
@@ -6,9 +6,11 @@ c = 3.0e8
 hbar = 6.58e-16
 m = 0.511e6
 
-cost = 1
+cost = math.sqrt(m)/(math.sqrt(2)*hbar*c*1e10)
 
+print(cost)
 
+f = math.cos(cost*math.sqrt(c0)*a*math.sqrt(x)) - math.sqrt(x/pot)
 
 ''' commento
 print('scrivi')
